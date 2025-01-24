7dc47ca46b22fa53ef58bf64d6127ea8ba97d0a6
msk <1580231853@qq.com>
2025-01-24 18:18:40 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,44 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-import { bus, setupApp, preloadApp, startApp, destroyApp } from 'wujie'
+import { setupApp, preloadApp, startApp } from 'wujie'
 
 createApp(App).mount('#app')
 
+type ObjectString = {
+    [key: string]: any
+}
+
+const getUrl = (key: string) => {
+    const conf: ObjectString = {
+        Bank: {
+            dev: '//localhost:5533/',
+            prod: '//47.113.180.39/bank/'
+        },
+        Grimoire: {
+            dev: '//localhost:5544/',
+            prod: '//47.113.180.39/grimoire/'
+        }
+    }
+    if (import.meta.env.NODE_ENV === 'development') {
+        return conf[key].dev
+    } else {
+        return conf[key].prod
+    }
+}
+
 const bankConfig = {
     name: 'Bank',
-    url: 'http://localhost:5173/',
+    url: getUrl('Bank'),
+    exec: true,
+    el: '#sub-app',
+    sync: true,
+    alive: true
+}
+
+const grimoireConfig = {
+    name: 'Grimoire',
+    url: getUrl('Grimoire'),
     exec: true,
     el: '#sub-app',
     sync: true,
@@ -16,5 +47,7 @@ const bankConfig = {
 }
 
 setupApp(bankConfig)
+setupApp(grimoireConfig)
 preloadApp(bankConfig)
+preloadApp(grimoireConfig)
 startApp(bankConfig)
