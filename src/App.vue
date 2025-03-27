1883fe2c8430fbc0e6d3e2a2c274bf77b318ea2e
msk <1580231853@qq.com>
2025-03-27 17:13:36 +0800

    恢复g项目

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
-<script setup lang="ts">
-</script>
-
 <template>
     <div>
-        <header></header>
+        <header>
+            <div @click="toProject(1)">项目一</div>
+            <div @click="toProject(2)">项目二</div>
+        </header>
         <div id="sub-app"></div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { startApp } from 'wujie'
+import { bankConfig, grimoireConfig } from './stores'
+const toProject = (type: number) => {
+    if (type === 1) {
+        startApp(bankConfig)
+    } else {
+        startApp(grimoireConfig)
+    }
+}
+</script>
 
 <style scoped>
 header {
     height: 4rem;
     background-color: antiquewhite;
+    > div {
+        cursor: pointer;
+    }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,48 +3,9 @@ import './style.css'
 import App from './App.vue'
 
 import { setupApp, preloadApp, startApp } from 'wujie'
+import { bankConfig, grimoireConfig } from './stores'
 
 createApp(App).mount('#app')
-
-type ObjectString = {
-    [key: string]: any
-}
-
-const getUrl = (key: string) => {
-    const conf: ObjectString = {
-        Bank: {
-            dev: '//localhost:5533/',
-            prod: '//47.113.180.39/bank/'
-        },
-        Grimoire: {
-            dev: '//localhost:5544/',
-            prod: '//47.113.180.39/grimoire/'
-        }
-    }
-    if (import.meta.env.NODE_ENV === 'development') {
-        return conf[key].dev
-    } else {
-        return conf[key].prod
-    }
-}
-
-const bankConfig = {
-    name: 'Bank',
-    url: getUrl('Bank'),
-    exec: true,
-    el: '#sub-app',
-    sync: true,
-    alive: true
-}
-
-const grimoireConfig = {
-    name: 'Grimoire',
-    url: getUrl('Grimoire'),
-    exec: true,
-    el: '#sub-app',
-    sync: true,
-    alive: true
-}
 
 setupApp(bankConfig)
 setupApp(grimoireConfig)
